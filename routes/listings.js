1925863fb101d622082cfae54ce90aa005e6e247
Simeon Pennisi <simeonpennisi@gmail.com>
2026-01-10 07:36:16 -0500

    imported requireAdmin to listings.js

--- routes/listings.js
+++ routes/listings.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { requireAuth } from "../middleware/auth.js";
+import { requireAdmin } from "../middleware/admin.js";
 
 const router = express.Router();
 const listings = [
